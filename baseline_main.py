b53cde9ba40f24bd1f00b3f15b401132ed008c50
breboana <87069910+breboana@users.noreply.github.com>
2021-07-13 17:10:14 +0200

    Add files via upload

--- baseline_main.py
+++ baseline_main.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     device = 'cuda' if args.gpu else 'cpu'
 
     SEED = args.seed
-    BATCH_SIZE = args.BATCH_SIZE
-    DATA_DIR = args.DATA_DIR
+    BATCH_SIZE = args.batch_size
+    DATA_DIR = args.data_dir
 
     random.seed(SEED)
     np.random.seed(SEED)
@@ -27,21 +27,17 @@ if __name__ == '__main__':
     torch.backends.cudnn.deterministic = True
 
     # load datasets
-    trainloader, validloader = get_train_valid_loader(DATA_DIR, batch_size=BATCH_SIZE,
-                                                      random_seed=SEED,
+    trainloader, validloader = get_train_valid_loader(args,
                                                       valid_size=0.2,
                                                       shuffle=True,
-                                                      num_workers=4,
                                                       pin_memory=False)
-    testloader = get_test_loader(DATA_DIR, batch_size=BATCH_SIZE,
+    testloader = get_test_loader(args,
                                  shuffle=True,
-                                 num_workers=4,
                                  pin_memory=False)
 
     # BUILD MODEL
     args.model = 'cnn'
     # Convolutional neural network
-    args.dataset = 'cifar'
     global_model = CNN(args=args)
 
     # Set the model to train and send it to device.
@@ -64,11 +60,13 @@ if __name__ == '__main__':
 
     epoch_accuracy_train = []
     epoch_accuracy_valid = []
+    epoch_accuracy_test = []
 
     loss_valid = 0
 
     epoch_loss_train = []
     epoch_loss_valid = []
+    epoch_loss_test  = []
 
     for epoch in range(args.epochs):
         batch_loss_train = []
@@ -103,8 +101,12 @@ if __name__ == '__main__':
         total = 0
 
         accuracy_valid, loss_valid = validation(args, global_model, validloader)
+        accuracy_test, loss_test = test_inference(args, global_model, testloader)
         epoch_loss_valid.append(loss_valid)
         epoch_accuracy_valid.append(accuracy_valid)
+        epoch_loss_test.append(loss_test)
+        epoch_accuracy_test.append(accuracy_test)
+
         print('Valid accuracy : {:.2f}%'.format(accuracy_valid), 'Valid loss : {:.4f}'.format(loss_valid))
 
 
@@ -112,18 +114,20 @@ if __name__ == '__main__':
 
     train_loss, = ax1.plot(range(len(epoch_loss_train)), epoch_loss_train)
     valid_loss, = ax1.plot(range(len(epoch_loss_valid)), epoch_loss_valid)
-    ax1.legend([train_loss, valid_loss], ['train loss', 'valid loss'])
+    test_loss, = ax1.plot(range(len(epoch_loss_test)), epoch_loss_test)
+    ax1.legend([train_loss, valid_loss], ['train loss', 'valid loss', 'test loss'])
     ax1.set_xlabel("Epoch")
     ax1.set_ylabel("Loss")
     train_acc, = ax2.plot(range(len(epoch_accuracy_train)), epoch_accuracy_train)
     valid_acc, = ax2.plot(range(len(epoch_accuracy_valid)), epoch_accuracy_valid)
-    ax2.legend([train_acc, valid_acc], ['train accuracy', 'valid accuracy'])
+    test_acc, = ax2.plot(range(len(epoch_accuracy_test)), epoch_accuracy_test)
+    ax2.legend([train_acc, valid_acc], ['train accuracy', 'valid accuracy', 'test accuracy'])
     ax2.set_xlabel("Epoch")
     ax2.set_ylabel("Accuracy")
 
 
     plt.savefig(
-        'C:/Users/Oana Madalina Breban/Desktop/federated_new_version/accuracy_loss_baseline.png'.format(args.dataset,
+        'C:/Users/Oana Madalina Breban/Downloads/FederatedLearning-main/FederatedLearning-main/accuracy_loss_baseline.png'.format(args.dataset,
                                                                                                         args.model,
                                                                                                         args.epochs))
     # testing
